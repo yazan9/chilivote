0fd90ed0ad3f7d6d7b299eca345a3b03a8f4f356
Yazan Khalaileh <yazan_rus@hotmail.com>
2014-05-05 18:49:40 +0300

    initial skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chilivote::Application.config.secret_key_base = '476fbd96b6d21e0fa286820cc05d9e924af13f4a3c621c394e5ffd75ea3323b74ca13ef73946961c8f51528d11878fd74c139ba4aafe7e14e618b4d433bf18e4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Chilivote::Application.config.session_store :cookie_store, key: '_chilivote_session'
