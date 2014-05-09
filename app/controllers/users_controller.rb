ac6a47d03eb56f0a58b105120cc7b6967dfd4ed2
Yazan Khalaileh <yazan_rus@hotmail.com>
2014-05-09 12:07:27 +0300

    completed user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :gender)
+      params.require(:user).permit(:first_name, :last_name, :email, :gender, :password, :password_confirmation)
     end
 end
