91ca8f30f9487de15280fd757d273ffb7a73b6ac
Yazan Khalaileh <yazan_rus@hotmail.com>
2015-03-08 22:30:00 +0200

    fixing urget error

--- app/controllers/pvotes_controller.rb
+++ app/controllers/pvotes_controller.rb
@@ -5,7 +5,7 @@ class PvotesController < ApplicationController
       @poll = Poll.find_by_id(params[:poll][:id])
       @option = @poll.vote_options.find_by_id(params[:vote_option][:id])
       if @option && @poll && !current_user.voted_for?(@poll)
-        @option.pvotes.create({user_id: current_user.id, poll_id:@poll.id})
+        @option.pvotes.create({user_id: current_user.id, poll_id: @poll.id})
       else
         render js: 'alert(\'Your vote cannot be saved. Have you already voted?\');'
       end
