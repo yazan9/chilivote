ab97748b26776d8dd5518214acb1d5f0df3fa1f9
yazan_rus@hotmail.com <yazan_rus@hotmail.com>
2015-10-21 12:51:15 +0000

    working with emails 5

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,8 @@ Chilivote::Application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = {:host => "localhost:3000"}
+  #config.action_mailer.default_url_options = {:host => "localhost:3000"}
+  config.action_mailer.default_url_options = {:host => "www.chilivote.com"}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
