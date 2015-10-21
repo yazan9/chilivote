dc11d0f93d6a85b1c01ae8568cb4940734b6cf6a
yazan_rus@hotmail.com <yazan_rus@hotmail.com>
2015-10-21 13:17:14 +0000

    emails 10

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,17 +79,4 @@ Chilivote::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = {:host => "localhost:3000"}
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :domain => "gmail.com",
-    :authentication => :plain,
-    :enable_starttls_auto => true,
-    :user_name => "yazan.khalaileh@gmail.com",
-    :password => "illigator140783"
-    }
 end
