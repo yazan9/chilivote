7ae2267b5a5a38254496508d7b8df9c44405a091
yazan_rus@hotmail.com <yazan_rus@hotmail.com>
2015-10-22 16:21:53 +0000

    mails 11

--- config/initializers/smtp_gmail.rb
+++ config/initializers/smtp_gmail.rb
@@ -2,7 +2,7 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :user_name            => "yazan.khalaileh@gmail.com",
-  :password             => "illigatorq14071983",
+  :password             => "illigator14071983",
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
