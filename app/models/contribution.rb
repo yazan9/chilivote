c8acef8dfa94b84981a264fbe7503c566940d339
yazan_rus@hotmail.com <yazan_rus@hotmail.com>
2016-07-21 18:42:05 +0000

    Alla yostor

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -3,6 +3,7 @@ class Contribution < ActiveRecord::Base
   has_many :likes, :foreign_key => :target_id, dependent: :destroy
   has_many :options, dependent: :destroy
   has_many :comments, :foreign_key => :cvote_id, dependent: :destroy
+  #Opppaaa
   
   def self.search_chilivotes(q)
     Contribution.where(['lower(title) LIKE ? AND contribution_type = ?', "%#{q}%", Chilivote::Application.config.contribution_type_cvote])
