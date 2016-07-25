9eb344d2a5e7660378fac1e18ce176456346ed1a
yazan_rus@hotmail.com <yazan_rus@hotmail.com>
2016-07-25 16:53:39 +0000

    fixing capitals

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,10 +4,10 @@ class SearchController < ApplicationController
     @current_user = current_user
     @my_friends = @current_user.friends
     @user = @current_user
-    if params[:q]
-      @users_results = User.search(params[:q])
-      @chilivotes_results = Contribution.search_chilivotes(params[:q])
-      @statuses_results = Contribution.search_statuses(params[:q])
+    if params[:q] and params[:q] != ""
+      @users_results = User.search(params[:q].downcase)
+      @chilivotes_results = Contribution.search_chilivotes(params[:q].downcase)
+      @statuses_results = Contribution.search_statuses(params[:q].downcase)
     end
   end
 end
