2d490502005aa789ffddebe0b31644de76ab753a
yazan9 <yazan.khalaileh@gmail.com>
2018-10-08 15:17:04 -0700

    Update smtp_gmail.rb

--- config/initializers/smtp_gmail.rb
+++ config/initializers/smtp_gmail.rb
@@ -2,7 +2,7 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :user_name            => "admin@chilivote.com",
-  :password             => "adminchilivote",
+  :password             => "####",
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
